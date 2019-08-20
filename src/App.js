bf1d5748d2c3bb2c7f8ae1a66376ac0764781ae8
Graham Hewett <hewettgraham@gmail.com>
2019-08-20 15:10:48 +0100

    grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import './App.css';
-import main from './bk.jpg';
+
+function updateTransition() {
+  var el = document.querySelector("div.box");
+   
+  if (el) {
+    el.className = "box1";
+  } else {
+    el = document.querySelector("div.box1");
+    el.className = "box";
+  }
+   
+  return el;
+}
+
+var intervalID = window.setInterval(updateTransition, 7000);
 
 function App() {
   return (
     <div className="App">
       <div className='cover'>
        <div className='box'></div>
+       
       </div>
-      <h1 className='title'>Sonia Sanchez</h1>
-    
+        <div class="parent">
+          <div class="box"> 
+            <h1 className='title'>Sonia Sanchez</h1>
+          </div>
+        </div>
+        <div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  
+</div>
+
     </div>
   );
 }
