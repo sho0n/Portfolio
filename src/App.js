97dd93fdbaf5ae25f83bbd026fd8b2bf634bce85
shon <shon.sanches@gmail.com>
2019-09-01 22:45:58 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function updateTransition() {
+  var el = document.querySelector("div.box");
+   
+  if (el) {
+    el.className = "box1";
+  } else {
+    el = document.querySelector("div.box1");
+    el.className = "box";
+  }
+   
+  return el;
+}
+
+var intervalID = window.setInterval(updateTransition, 7000);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='cover'>
+       <div className='box'></div>
+       
+      </div>
+        <div class="parent">
+          <div class="box"> 
+            <h1 className='title'>Sonia Sanchez</h1>
+          </div>
+        </div>
+        <div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  
+</div>
+
     </div>
   );
 }
