7d85bcd349587c99579cb604a503023d93dd7cc9
Shon Sanches <Shon@Shons-MacBook-Pro.local>
2020-08-18 13:42:22 +0200

    added projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,6 +5,10 @@ function Projects() {
     return (
         <div className='projContainer'>
             <div className='proj'>
+            <h1><a className='projectLink' href='https://sho0n.github.io/react-weather/'>React-weather</a></h1>
+            <iframe src="https://sho0n.github.io/react-weather/" title='weather'/>
+            </div>
+            <div className='proj'>
             <h1><a className='projectLink' href='https://sho0n.github.io/react-tetris/'>React-Tetris</a></h1>
             <iframe src="https://sho0n.github.io/react-tetris/" title='tetris'/>
             </div>
@@ -12,10 +16,7 @@ function Projects() {
             <h1><a className='projectLink' href='https://kodflix-sonia.herokuapp.com/'>Kodflix</a></h1>
             <iframe src="https://kodflix-sonia.herokuapp.com/" title='kodflix'/>
             </div>
-            <div className='proj'>
-            <h1>MERN</h1>
-            <iframe src="" title='mern'/>
-            </div>
+
         </div>
     );
 }
