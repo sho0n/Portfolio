107dad12986ae3214e09338f35e0598b30c68449
Sho0n <shon.sanches@gmail.com>
2021-01-25 12:20:10 +0100

    Updates

---
+++ precache-manifest.1c196e16e053939687a7586d6aad4429.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63264bcc9228275487cfe2cd60b261e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc394767568a7325c5eb",
+    "url": "/portfolio/static/css/main.6474f016.chunk.css"
+  },
+  {
+    "revision": "2c0d893d69d831a2d02b",
+    "url": "/portfolio/static/js/2.545fae4b.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/portfolio/static/js/2.545fae4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc394767568a7325c5eb",
+    "url": "/portfolio/static/js/main.3cbc29f6.chunk.js"
+  },
+  {
+    "revision": "f9ebd07c5c34b5ecd0d4",
+    "url": "/portfolio/static/js/runtime-main.d4e7964a.js"
+  },
+  {
+    "revision": "8291284c2aaf85892b0474cb9c02cee4",
+    "url": "/portfolio/static/media/cssIcon.8291284c.png"
+  },
+  {
+    "revision": "7fa80d12263e1acbfc0af3c0e72f6085",
+    "url": "/portfolio/static/media/htmlIcon.7fa80d12.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/portfolio/static/media/javaIcon.652fdb65.png"
+  },
+  {
+    "revision": "608be566b0e69846cb2001d867640bed",
+    "url": "/portfolio/static/media/jsIcon.608be566.png"
+  },
+  {
+    "revision": "84dae0c85887a8c7798e1a0df84fd34e",
+    "url": "/portfolio/static/media/nodeIcon.84dae0c8.png"
+  },
+  {
+    "revision": "6bbb347fdd7aee52f468986e1a1dd8cf",
+    "url": "/portfolio/static/media/reactIcon.6bbb347f.png"
+  },
+  {
+    "revision": "7fc30f431b8beefb1095072f75d3257a",
+    "url": "/portfolio/static/media/uk.7fc30f43.png"
+  }
+]);
